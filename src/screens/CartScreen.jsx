6a5369cc762ab95cb6ec751b6be3f2acc385f4a4
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-04-11 10:01:38 +0530

    Checkout Warning

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -324,6 +324,18 @@ const CartScreen = () => {
                 </div>
               )}
               <div className="mt-2">
+                <div className="alert alert-warning" role="alert">
+                  Payment is in testing mode. Please avoid filling personal
+                  sensitive information. Use card numbers provided{" "}
+                  <a
+                    href="https://developer.paypal.com/braintree/docs/guides/credit-cards/testing-go-live/php/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    here
+                  </a>
+                  .
+                </div>
                 {clientToken && cartItems?.length > 0 && (
                   <>
                     <DropIn
