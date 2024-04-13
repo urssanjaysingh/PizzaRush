0b3415203264ad7ccb544eaa007429dceb15554c
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-04-13 10:07:25 +0530

    Cart

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -159,10 +159,7 @@ const CartScreen = () => {
                           </div>
                         </div>
                         <div className="col-md-7 d-flex align-items-center justify-content-center">
-                          <div
-                            className="card-body mt-3"
-                            style={{ margin: "auto 0" }}
-                          >
+                          <div className="card-body d-flex flex-column justify-content-center">
                             <p
                               style={{
                                 margin: 0,
