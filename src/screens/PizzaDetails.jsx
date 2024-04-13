4fd23de5ae382b345e3eee181073f88bff5f42cb
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-04-13 11:36:57 +0530

    Card

--- src/screens/PizzaDetails.jsx
+++ src/screens/PizzaDetails.jsx
@@ -125,54 +125,49 @@ const PizzaDetails = () => {
                 ) : relatedPizzas.length < 1 ? (
                   <p className="text-center">No Similar Pizzas Found</p>
                 ) : (
-                  <div
-                    className="d-flex flex-wrap fade-in justify-content-center mt-2"
-                    style={{ marginLeft: "-5px", marginRight: "-5px" }}
-                  >
+                  <div className="d-flex flex-wrap fade-in justify-content-center">
                     {relatedPizzas?.map((p, i) => (
-                      <div key={i} className="col-sm-6 col-md-4 col-lg-3 p-2">
-                        <div
-                          className="card product-card m-2 bg-light"
-                          style={{ width: "18rem" }}
-                        >
-                          <div className="square-container">
-                            <img
-                              className="card-img-top product-image"
-                              src={p.image}
-                              alt="Card"
-                              style={{ cursor: "pointer" }}
-                              onClick={() => handleShow(p)}
-                            />
+                      <div
+                        className="card product-card m-2 bg-light"
+                        style={{ width: "18rem" }}
+                      >
+                        <div className="square-container">
+                          <img
+                            className="card-img-top product-image"
+                            src={p.image}
+                            alt="Card"
+                            style={{ cursor: "pointer" }}
+                            onClick={() => handleShow(p)}
+                          />
+                        </div>
+                        <div className="card-body">
+                          <div className="product-info d-flex align-items-center justify-content-between">
+                            <h5 className="card-title product-title">
+                              {p.name}
+                            </h5>
+                            <p className="product-price">₹{p.price}</p>
                           </div>
-                          <div className="card-body">
-                            <div className="product-info d-flex align-items-center justify-content-between">
-                              <h5 className="card-title product-title">
-                                {p.name}
-                              </h5>
-                              <p className="product-price">₹{p.price}</p>
-                            </div>
-                            <p
-                              className="card-text product-description"
-                              style={{ textAlign: "justify" }}
+                          <p
+                            className="card-text product-description"
+                            style={{ textAlign: "justify" }}
+                          >
+                            {p.description.length > 30
+                              ? p.description.substring(0, 60) + "..."
+                              : p.description}
+                          </p>
+                          <hr />
+                          <div className="text-center">
+                            <button
+                              className="btn product-button"
+                              style={{
+                                padding: "0.5rem 1rem",
+                                fontSize: "0.8rem",
+                              }} // Adjust button size
+                              onClick={() => addToCartHandler(p)}
                             >
-                              {p.description.length > 30
-                                ? p.description.substring(0, 60) + "..."
-                                : p.description}
-                            </p>
-                            <hr />
-                            <div className="text-center">
-                              <button
-                                className="btn product-button"
-                                style={{
-                                  padding: "0.5rem 1rem",
-                                  fontSize: "0.8rem",
-                                }} // Adjust button size
-                                onClick={() => addToCartHandler(p)}
-                              >
-                                Add to Cart{" "}
-                                <i className="fas fa-shopping-cart me-1"></i>
-                              </button>
-                            </div>
+                              Add to Cart{" "}
+                              <i className="fas fa-shopping-cart me-1"></i>
+                            </button>
                           </div>
                         </div>
                       </div>
