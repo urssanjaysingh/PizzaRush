8be56ca879ddcfd5dd72d305b345740c956d96ea
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-04-13 07:54:28 +0530

    Cart

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -117,252 +117,250 @@ const CartScreen = () => {
   return (
     <>
       <Layout title={"Pizzarush - Cart"}>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <h1 className="text-center bg-light p-2 mb-1">
-                {currentUser?.token && currentUser?.user?.name
-                  ? `Hello ${currentUser.user.name}`
-                  : "Hello"}
-              </h1>
-              <h4 className="text-center mb-4">
-                {cartItems?.length >= 1
-                  ? `You have ${cartItems.length} items in your cart`
-                  : "Your cart is empty"}
-              </h4>
-            </div>
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="text-center bg-light p-2 mb-1">
+              {currentUser?.token && currentUser?.user?.name
+                ? `Hello ${currentUser.user.name}`
+                : "Hello"}
+            </h1>
+            <h4 className="text-center mb-4">
+              {cartItems?.length >= 1
+                ? `You have ${cartItems.length} items in your cart`
+                : "Your cart is empty"}
+            </h4>
           </div>
-          <div className="row">
-            <div className="col-md-8">
-              <div className="row">
-                <div className="d-flex flex-wrap fade-in justify-content-center">
-                  {cartItems
-                    .slice()
-                    .reverse()
-                    .map((pizza, index) => (
-                      <div
-                        className="card mb-3 d-flex justify-content-center"
-                        style={{ paddingLeft: "10px" }}
-                        key={index}
-                      >
-                        <div className="row">
-                          <div className="col-md-3 d-flex align-items-center justify-content-center">
-                            <img
-                              src={process.env.PUBLIC_URL + pizza.image}
-                              className="img-fluid"
-                              alt={pizza.name}
-                            />
-                          </div>
-                          <div className="col-md-9 d-flex align-items-center justify-content-center">
-                            <div className="card-body mt-3">
-                              <p
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="d-flex flex-wrap fade-in justify-content-center">
+                {cartItems
+                  .slice()
+                  .reverse()
+                  .map((pizza, index) => (
+                    <div
+                      className="card mb-3 d-flex justify-content-center"
+                      style={{ paddingLeft: "10px" }}
+                      key={index}
+                    >
+                      <div className="row">
+                        <div className="col-md-3 d-flex align-items-center justify-content-center">
+                          <img
+                            src={process.env.PUBLIC_URL + pizza.image}
+                            className="img-fluid"
+                            alt={pizza.name}
+                          />
+                        </div>
+                        <div className="col-md-9 d-flex align-items-center justify-content-center">
+                          <div className="card-body mt-3">
+                            <p
+                              style={{
+                                margin: 0,
+                                fontWeight: "bold",
+                                fontSize: "20px",
+                              }}
+                            >
+                              {pizza.name}
+                            </p>
+                            <p
+                              className="product-description"
+                              style={{ textAlign: "justify" }}
+                            >
+                              {pizza.description}
+                            </p>
+                            <div className="quantity-controls">
+                              <span
                                 style={{
-                                  margin: 0,
                                   fontWeight: "bold",
-                                  fontSize: "20px",
+                                  marginTop: "8px",
+                                  color: "lightSalmon",
                                 }}
                               >
-                                {pizza.name}
-                              </p>
-                              <p
-                                className="product-description"
-                                style={{ textAlign: "justify" }}
+                                Quantity:
+                              </span>
+                              <button
+                                className="btn"
+                                style={{ border: "none", cursor: "pointer" }}
+                                onClick={() => decreaseQuantity(pizza)} // Decrease quantity button
                               >
-                                {pizza.description}
-                              </p>
-                              <div className="quantity-controls">
+                                <FontAwesomeIcon
+                                  icon={faMinusCircle}
+                                  style={{
+                                    fontSize: "1.2rem",
+                                    color: "#FF8888",
+                                  }}
+                                />
+                              </button>
+                              <span
+                                className="quantity"
+                                style={{
+                                  fontWeight: "bold",
+                                  marginTop: "8px",
+                                }}
+                              >
+                                {pizza.quantity}
+                              </span>
+                              <button
+                                className="btn"
+                                style={{ border: "none", cursor: "pointer" }}
+                                onClick={() => increaseQuantity(pizza)} // Increase quantity button
+                              >
+                                <FontAwesomeIcon
+                                  icon={faPlusCircle}
+                                  style={{
+                                    fontSize: "1.2rem",
+                                    color: "green",
+                                  }}
+                                />
+                                {/* Adjusted icon size and color */}
+                              </button>{" "}
+                              <p>
                                 <span
                                   style={{
                                     fontWeight: "bold",
                                     marginTop: "8px",
-                                    color: "lightSalmon",
+                                    color: "salmon",
                                   }}
                                 >
-                                  Quantity:
+                                  Price: ₹{pizza.price}
                                 </span>
+                                &nbsp;&nbsp;&nbsp;&nbsp;
+                                <span
+                                  style={{
+                                    fontWeight: "bold",
+                                    marginTop: "8px",
+                                    color: "tomato",
+                                  }}
+                                >
+                                  Total: ₹{pizza.price * pizza.quantity}{" "}
+                                </span>{" "}
+                                &nbsp;&nbsp;&nbsp;&nbsp;
                                 <button
                                   className="btn"
-                                  style={{ border: "none", cursor: "pointer" }}
-                                  onClick={() => decreaseQuantity(pizza)} // Decrease quantity button
+                                  style={{
+                                    border: "none",
+                                    cursor: "pointer",
+                                  }}
+                                  onClick={() =>
+                                    dispatch(deleteFromCart(pizza))
+                                  }
                                 >
                                   <FontAwesomeIcon
-                                    icon={faMinusCircle}
+                                    icon={faTrash}
                                     style={{
                                       fontSize: "1.2rem",
-                                      color: "#FF8888",
+                                      color: "#FF4444",
                                     }}
                                   />
                                 </button>
-                                <span
-                                  className="quantity"
-                                  style={{
-                                    fontWeight: "bold",
-                                    marginTop: "8px",
-                                  }}
-                                >
-                                  {pizza.quantity}
-                                </span>
-                                <button
-                                  className="btn"
-                                  style={{ border: "none", cursor: "pointer" }}
-                                  onClick={() => increaseQuantity(pizza)} // Increase quantity button
-                                >
-                                  <FontAwesomeIcon
-                                    icon={faPlusCircle}
-                                    style={{
-                                      fontSize: "1.2rem",
-                                      color: "green",
-                                    }}
-                                  />
-                                  {/* Adjusted icon size and color */}
-                                </button>{" "}
-                                <p>
-                                  <span
-                                    style={{
-                                      fontWeight: "bold",
-                                      marginTop: "8px",
-                                      color: "salmon",
-                                    }}
-                                  >
-                                    Price: ₹{pizza.price}
-                                  </span>
-                                  &nbsp;&nbsp;&nbsp;&nbsp;
-                                  <span
-                                    style={{
-                                      fontWeight: "bold",
-                                      marginTop: "8px",
-                                      color: "tomato",
-                                    }}
-                                  >
-                                    Total: ₹{pizza.price * pizza.quantity}{" "}
-                                  </span>{" "}
-                                  &nbsp;&nbsp;&nbsp;&nbsp;
-                                  <button
-                                    className="btn"
-                                    style={{
-                                      border: "none",
-                                      cursor: "pointer",
-                                    }}
-                                    onClick={() =>
-                                      dispatch(deleteFromCart(pizza))
-                                    }
-                                  >
-                                    <FontAwesomeIcon
-                                      icon={faTrash}
-                                      style={{
-                                        fontSize: "1.2rem",
-                                        color: "#FF4444",
-                                      }}
-                                    />
-                                  </button>
-                                </p>
-                              </div>
+                              </p>
                             </div>
                           </div>
                         </div>
                       </div>
-                    ))}
-                </div>
+                    </div>
+                  ))}
               </div>
             </div>
-            <div className="col-md-4 text-center">
-              <h2>Cart Summary</h2>
-              <p>Checkout | Payment</p>
-              <hr />
-              <span
-                style={{
-                  fontSize: "25px",
-                  fontWeight: "bold",
-                  marginTop: "8px",
-                  color: "DarkSalmon",
-                }}
-              >
-                Grand Total : {totalPrice()}
-              </span>
-              {currentUser?.user?.address ? (
-                <>
-                  <div className="mb">
-                    <h5>Current Address</h5>
-                    <h6>{currentUser?.user?.address}</h6>
-                    <button
-                      className="btn btn-outline-warning"
-                      onClick={() =>
-                        navigate("/dashboard/user/profile", {
-                          state: { from: "/cart" },
-                        })
-                      }
-                    >
-                      Update Address
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <div className="mb-3">
-                  {currentUser?.token ? (
-                    <button
-                      className="btn btn-outline-warning"
-                      onClick={() =>
-                        navigate("/dashboard/user/profile", {
-                          state: { from: "/cart" },
-                        })
-                      }
-                    >
-                      Update Address
-                    </button>
-                  ) : (
-                    <button
-                      className="btn btn-outline-warning"
-                      onClick={() =>
-                        navigate("/login", {
-                          state: { from: "/cart" },
-                        })
-                      }
-                    >
-                      Please Login To Checkout
-                    </button>
-                  )}
+          </div>
+          <div className="col-md-4 text-center">
+            <h2>Cart Summary</h2>
+            <p>Checkout | Payment</p>
+            <hr />
+            <span
+              style={{
+                fontSize: "25px",
+                fontWeight: "bold",
+                marginTop: "8px",
+                color: "DarkSalmon",
+              }}
+            >
+              Grand Total : {totalPrice()}
+            </span>
+            {currentUser?.user?.address ? (
+              <>
+                <div className="mb">
+                  <h5>Current Address</h5>
+                  <h6>{currentUser?.user?.address}</h6>
+                  <button
+                    className="btn btn-outline-warning"
+                    onClick={() =>
+                      navigate("/dashboard/user/profile", {
+                        state: { from: "/cart" },
+                      })
+                    }
+                  >
+                    Update Address
+                  </button>
                 </div>
-              )}
-              <div className="mt-2">
-                {clientToken && cartItems?.length > 0 && (
-                  <>
-                    <div
-                      className="alert alert-warning"
-                      role="alert"
-                      style={{ fontSize: "small" }}
-                    >
-                      Payment is currently in testing mode. Please use the
-                      following card numbers provided{" "}
-                      <a
-                        href="https://developer.paypal.com/braintree/docs/guides/credit-cards/testing-go-live/php/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        here
-                      </a>
-                      .
-                    </div>
-                    <DropIn
-                      options={{
-                        authorization: clientToken,
-                        paypal: false,
-                      }}
-                      onInstance={(instance) => {
-                        setInstance(instance);
-                      }}
-                    />
-                    <button
-                      className="btn btn-primary"
-                      onClick={handlePayment}
-                      disabled={
-                        loading || !instance || !currentUser?.user?.address
-                      }
-                    >
-                      {loading ? "Processing ...." : "Make Payment"}
-                    </button>
-                  </>
+              </>
+            ) : (
+              <div className="mb-3">
+                {currentUser?.token ? (
+                  <button
+                    className="btn btn-outline-warning"
+                    onClick={() =>
+                      navigate("/dashboard/user/profile", {
+                        state: { from: "/cart" },
+                      })
+                    }
+                  >
+                    Update Address
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-outline-warning"
+                    onClick={() =>
+                      navigate("/login", {
+                        state: { from: "/cart" },
+                      })
+                    }
+                  >
+                    Please Login To Checkout
+                  </button>
                 )}
               </div>
+            )}
+            <div className="mt-2">
+              {clientToken && cartItems?.length > 0 && (
+                <>
+                  <div
+                    className="alert alert-warning"
+                    role="alert"
+                    style={{ fontSize: "small" }}
+                  >
+                    Payment is currently in testing mode. Please use the
+                    following card numbers provided{" "}
+                    <a
+                      href="https://developer.paypal.com/braintree/docs/guides/credit-cards/testing-go-live/php/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      here
+                    </a>
+                    .
+                  </div>
+                  <DropIn
+                    options={{
+                      authorization: clientToken,
+                      paypal: false,
+                    }}
+                    onInstance={(instance) => {
+                      setInstance(instance);
+                    }}
+                  />
+                  <button
+                    className="btn btn-primary"
+                    onClick={handlePayment}
+                    disabled={
+                      loading || !instance || !currentUser?.user?.address
+                    }
+                  >
+                    {loading ? "Processing ...." : "Make Payment"}
+                  </button>
+                </>
+              )}
             </div>
           </div>
         </div>
